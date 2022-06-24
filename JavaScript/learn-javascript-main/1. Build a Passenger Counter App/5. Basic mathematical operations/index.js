0da476b4d67d7e9255939c4008cd06c768b0dfb0
Dieguiini <dgajardor@gmail.com>
2022-06-24 15:54:05 -0400

    5

--- JavaScript/learn-javascript-main/1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
+++ JavaScript/learn-javascript-main/1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
@@ -1,6 +1,6 @@
 // document.getElementById("count").innerText = 5
 
-// cmd+k+c
+// ctrl+shift+alt
 // let firstBatch = 5
 // let secondBatch = 7
 // let count = firstBatch + secondBatch
@@ -11,3 +11,8 @@
 // 2. Multiply the two together and store the result in myDogAge 
 // 3. Log myDogAge to the console
 
+let myAge = 35
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
