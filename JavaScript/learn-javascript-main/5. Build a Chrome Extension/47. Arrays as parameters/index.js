41cc7d53eecabcb6ac8b7edeec07843e4bc62d27
Dieguiini <dgajardor@gmail.com>
2022-07-04 12:04:52 -0400

    build a chrome extension

--- JavaScript/learn-javascript-main/5. Build a Chrome Extension/47. Arrays as parameters/index.js
+++ JavaScript/learn-javascript-main/5. Build a Chrome Extension/47. Arrays as parameters/index.js
@@ -1,5 +1,12 @@
 // Create a function, getFirst(arr), that returns the first item in the array
 
+function getfirst(arr) {
+    return arr[0]
+}
 
 
 // Call it with an array as an argument to verify that it works
+
+let firstCard = getfirst([2, 4, 6])
+
+console.log(firstCard)
