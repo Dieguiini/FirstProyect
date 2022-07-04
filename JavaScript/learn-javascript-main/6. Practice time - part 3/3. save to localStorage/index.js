f1e1b7b01ef37708a5262bd4c0c36237893e3847
Dieguiini <dgajardor@gmail.com>
2022-07-04 13:48:08 -0400

    Practice time part 3

--- JavaScript/learn-javascript-main/6. Practice time - part 3/3. save to localStorage/index.js
+++ JavaScript/learn-javascript-main/6. Practice time - part 3/3. save to localStorage/index.js
@@ -1,3 +1,6 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+let myPages = localStorage.getItem("myPages")
+console.log(myPages)
